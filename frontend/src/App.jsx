316c788b71c30a0bc2572057fbb2040377d6a32b
vyakhyarastogi <vyakhyarastogi975@gmail.com>
2024-03-21 21:01:52 +0530

    styled navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,16 +6,16 @@ import { Events, Contact, Home, Profiles } from "./components/pages";
 
 function App() {
   return (
-      <div className='App'>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/profiles" element={<Profiles />} />
-        </Routes>
-        <Footer/>
-      </div>
+    <div className='App'>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/profiles" element={<Profiles />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,63 +9,65 @@ const Navbar = () => {
   const toggleMenu = () => {
     setShowMenu(!showMenu);
   };
- 
-   const closeMenuOnMobile = () => {
-     if (window.innerWidth <= 1150) {
-       setShowMenu(false);
-     }
-   };
- return (
-   <header className="header">
-     <nav className="nav container">
-       <NavLink to="/" className="nav__logo">
-         Blockchain Club
-       </NavLink>
 
-       <div className={`nav__menu ${showMenu ? "show-menu" : ""}`}
-         id="nav-menu">
-         <ul className="nav__list">
-           <li className="nav__item">
-             <NavLink to="/" className="nav__link"   onClick={closeMenuOnMobile}>
-               Home
-             </NavLink>
-           </li>
-           <li className="nav__item">
-             <NavLink to="/events" className="nav__link"   onClick={closeMenuOnMobile}>
-               Events
-             </NavLink>
-           </li>
-           <li className="nav__item">
-             <NavLink
-               to="/contact"
-               className="nav__link" 
-               onClick={closeMenuOnMobile}
-             >
-              Contact
-             </NavLink>
-           </li>
-           <li className="nav__item">
-             <NavLink
-               to="/profiles"
-               className="nav__link"
-               onClick={closeMenuOnMobile}
-             >
-               Profiles
-             </NavLink>
-           </li>
-           
-         </ul>
-         <div className="nav__close" id="nav-close" onClick={toggleMenu}>
-           <IoClose />
-         </div>
-       </div>
+  const closeMenuOnMobile = () => {
+    if (window.innerWidth <= 1150) {
+      setShowMenu(false);
+    }
+  };
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <NavLink to="/" className="nav__logo">
+          Blockchain Club
+        </NavLink>
 
-       <div className="nav__toggle" id="nav-toggle" onClick={toggleMenu}>
-         <IoMenu />
-       </div>
-     </nav>
-   </header>
- );
+        <div className={`nav__menu ${showMenu ? "show-menu" : ""}`}
+          id="nav-menu">
+          <ul className="nav__list">
+            <li className="nav__item">
+              <NavLink to="/" className="nav__link" onClick={closeMenuOnMobile}>
+                Home
+              </NavLink>
+            </li>
+            <li className="nav__item">
+              <NavLink to="/events" className="nav__link" onClick={closeMenuOnMobile}>
+                Events
+              </NavLink>
+            </li>
+            <li className="nav__item">
+              <NavLink
+                to="/contact"
+                className="nav__link"
+                onClick={closeMenuOnMobile}
+              >
+                Contact
+              </NavLink>
+            </li>
+            <li className="nav__item">
+              <NavLink
+                to="/profiles"
+                className="nav__link"
+                onClick={closeMenuOnMobile}
+              >
+                Profiles
+              </NavLink>
+            </li>
+
+          </ul>
+          <div className="nav__close" id="nav-close" onClick={toggleMenu}>
+            <IoClose />
+          </div>
+        </div>
+
+        <div className="nav__toggle" id="nav-toggle" onClick={toggleMenu}>
+          <IoMenu />
+        </div>
+
+        <button className="navBtn">Join Us</button>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
