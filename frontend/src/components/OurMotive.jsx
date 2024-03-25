3cb3ee5fc4ffb0d4a50644fcc3a3f7316510a1b4
Dinesh <82709381+Dinesht04@users.noreply.github.com>
2024-03-25 19:41:50 +0530

    Update OurMotive.jsx
    Made responsive for all screen sizes

--- frontend/src/components/OurMotive.jsx
+++ frontend/src/components/OurMotive.jsx
@@ -3,6 +3,7 @@ import Lottie from 'react-lottie';
 import InnovationAnimationData from '../assets/InnovationAnimationData.json'; 
 import ImpactAnimationData from '../assets/ImpactAnimationData.json';
 
+
 const OurMotive = () => {
 
     const InnovationAnimationOptions = {
@@ -10,71 +11,64 @@ const OurMotive = () => {
         autoplay: true, 
         animationData: InnovationAnimationData, 
         rendererSettings: {
-          preserveAspectRatio: 'xMidYMid slice' // aspect - ratio 
+          preserveAspectRatio: 'xMidYMid slice'
         }
-      };
+    };
 
-      const ImpactAnimationOptions = {
+    const ImpactAnimationOptions = {
         loop: true, 
         autoplay: true, 
         animationData: ImpactAnimationData, 
         rendererSettings: {
-          preserveAspectRatio: 'xMidYMid slice' //aspect - ratio ke liye
+          preserveAspectRatio: 'xMidYMid slice'
         }
-      };
+    };
 
-      const containerStyles = {
+    const containerStyles = {
         display: 'grid',
         gridTemplateRows: 'repeat(3, 1fr)',
-        gridTemplateColumns: 'repeat(2, 1fr)',
-        columnGap: '20px',
-        rowGap: '20px',
-      };
-      
+        gridTemplateColumns: 'repeat(1, 1fr)', 
+        gap: '20px',
+    };
 
     return (
         <div>
-            <body>
-                <div className="container mx-auto px-4 py-8">
-                    <div className="text-center mb-8" style={{
-                        textAlign: 'center',
-                        color: '#FFFF00'
-                    }}>
+            <div className="container mx-auto px-4 py-8">
+                <div className="text-center mb-8" style={{ color: '#FFFF00' }}>
                     <h1 className="text-4xl font-bold">Our Motive!</h1>
-                    </div>
+                </div>
                 <div id="container" style={containerStyles}>
-                    <div id='innovation-animation' className='col-start-2 row-start-2 col-end-3 row-end-3'>
+                    <div id='Empowerment' className='md:col-start-2 md:row-start-1 md:col-end-3 md:row-end-2'>
+                        <div className="text-lg font-bold mb-2 text-yellow-700">01.</div>
+                        <h2 className="text-xl font-bold mb-2 ">Empowerment</h2>   
+                        <p className="text-white">
+                            Our mission is to empower members with the knowledge and skills needed to navigate the blockchain landscape confidently. Through workshops, seminars, and projects, we equip individuals to become leaders in the field.
+                        </p>
+                    </div>
+                    <div id='innovation-animation' className='md:col-start-2 md:row-start-2 md:col-end-3 md:row-end-3'>
                         <Lottie options={InnovationAnimationOptions} height={400} width={400} />
                     </div>
-                <div id="innovation" className='col-start-1 row-start-2 col-end-2 row-end-3'>
-                    <div className="text-l font-bold mb-2 text-yellow-700">02.</div>
-                        <h2 className="text-2xl font-bold mb-2 text-left">Innovation</h2>
-                        <p className="text-white-700">
-                    We foster a culture of creativity and exploration, encouraging members to push the boundaries of blockchain technology. Collaborative projects enable us to be at the forefront of innovation in the blockchain space.
+                    <div id="innovation" className='md:col-start-1 md:row-start-2 md:col-end-2 md:row-end-3'>
+                        <div className="text-lg font-bold mb-2 text-yellow-700">02.</div>
+                        <h2 className="text-xl font-bold mb-2 text-left">Innovation</h2>
+                        <p className="text-white">
+                            We foster a culture of creativity and exploration, encouraging members to push the boundaries of blockchain technology. Collaborative projects enable us to be at the forefront of innovation in the blockchain space.
                         </p>
-                </div>
-                <div id="Empowerment" className='col-start-2 row-start-1 col-end-3 row-end-2'>
-                    <div className="text-l font-bold mb-2 text-yellow-700">01.</div>
-                    <h2 className="text-2xl font-bold mb-2 ">Empowerment</h2>   
-                    <p className="text-white-700">
-                    Our mission is to empower members with the knowledge and skills needed to navigate the blockchain landscape confidently. Through workshops, seminars, and projects, we equip individuals to become leaders in the field.
-                    </p>
-                </div>
-                <div id="impact" className='col-start-2 row-start-3 col-end-3 row-end-4'>
-                    <div className="text-l font-bold mb-2 text-yellow-700">03.</div>
-                    <h2 className="text-2xl font-bold mb-2">Impact</h2>
-                    <p className="text-white-700">
-                    We prioritize making a tangible difference with blockchain. Whether it's increasing transparency or promoting financial inclusion, our goal is to drive positive changes that improve lives. Join us in creating a better future through blockchain innovation.
-                    </p>
-                </div>
-                <div id='impact-animation' className='col-start-1 row-start-3 col-end-2 row-end-4'>
-                    <Lottie options={ImpactAnimationOptions} height={400} width={400} />
-                </div>
+                    </div>
+                    <div id='impact-animation' className='md:col-start-1 md:row-start-3 md:col-end-2 md:row-end-4'>
+                        <Lottie options={ImpactAnimationOptions} height={400} width={400} />
+                    </div>
+                    <div id="impact" className='md:col-start-2 md:row-start-3 md:col-end-3 md:row-end-4'>
+                        <div className="text-lg font-bold mb-2 text-yellow-700">03.</div>
+                        <h2 className="text-xl font-bold mb-2">Impact</h2>
+                        <p className="text-white">
+                            We prioritize making a tangible difference with blockchain. Whether it's increasing transparency or promoting financial inclusion, our goal is to drive positive changes that improve lives. Join us in creating a better future through blockchain innovation.
+                        </p>
+                    </div>
                 </div>
             </div>
-        </body>
-    </div>
-    )
+        </div>
+    );
 }
 
 export default OurMotive;
