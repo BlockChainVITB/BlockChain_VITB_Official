8ff93ad2bbb17edf5761f4acf40f66218f2870bc
Sagnik Sahoo <sahoosagnik1@gmail.com>
2024-03-27 21:26:30 +0530

    Made responsive

--- frontend/src/components/TeamDetails.jsx
+++ frontend/src/components/TeamDetails.jsx
@@ -14,7 +14,7 @@ const TeamDetails = () => {
 
     return (
         <div className="relative w-full overflow-hidden">
-            <div className="text-center">
+            <div className="text-center mb-6">
                 <h1 className="text-white text-4xl sm:text-5xl md:text-6xl">Team Members</h1>
             </div>
             {/* Wrapping div for seamless looping */}
@@ -33,9 +33,9 @@ const TeamDetails = () => {
                 {duplicatedSlides.map((slide, index) => (
                     <div key={index} className="flex-shrink-0" style={{ width: `${100 / slides.length}%` }} >{/* Add mr- for margin */}
                         <div className="flex-shrink-0">
-                            <div className="flex flex-col items-center justify-center h-full text-center">
-                                <div className="max-w-lg mx-auto my-10 bg-white rounded-lg shadow-md p-4 md:p-6 lg:p-8"> {/* Responsive padding */}
-                                    <img className="w-24 h-24 rounded-full mx-auto mb-4" src={slide.photo} alt={slide.name} />
+                        <div className="auto-fit-[120px] grid gap-6 p-6">
+                                 <div className="rounded-lg bg-white p-4 text-center shadow-md"> {/* Responsive padding */}
+                                    <img className="mx-auto mb-4 h-24 w-24 rounded-full" src={slide.photo} alt={slide.name} />
                                     <h2 className="text-xl font-semibold">{slide.name}</h2>
                                     <p className="text-sm text-gray-600">{slide.position}</p>
                                 </div>
